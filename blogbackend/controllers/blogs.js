7c80223e55c8bd5312d1f530d10c72a3c3b5f1a9
Mikko Lipsanen <mikko.lipsanen@helsinki.fi>
2019-02-15 15:20:23 +0200

    Tehtävä 4.10.

--- blogbackend/controllers/blogs.js
+++ blogbackend/controllers/blogs.js
@@ -5,16 +5,11 @@ blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs.map(blog => blog.toJSON()))
 })
-
   
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
-    
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const savedBlog = await blog.save()
+  response.json(savedBlog.toJSON())
 })
 
 module.exports = blogsRouter
