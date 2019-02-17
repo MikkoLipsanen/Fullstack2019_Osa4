7e61d00aacae1191714eb89ce8b6d00b42c803fa
Mikko Lipsanen <mikko.lipsanen@helsinki.fi>
2019-02-17 16:12:56 +0200

    Tehtävät 4.17., 4.18. ja 4.19.

--- blogbackend/controllers/users.js
+++ blogbackend/controllers/users.js
@@ -3,13 +3,16 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
-  response.json(users.map(u => u.toJSON()))
+  const users = await User
+    .find({}).populate('blogs', { title: 1, author: 1, url: 1, likes: 1 })
+
+  response.json(users.map(user => user.toJSON()))
 })
 
 usersRouter.post('/', async (request, response, next) => {
   try {
     const body = request.body
+
     if (body.password === undefined || body.password.length === 0) {
       return response.status(400).json({ error: 'password is missing' })
     }
--- blogbackend/utils/middleware.js
+++ blogbackend/utils/middleware.js
@@ -13,6 +13,11 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
   }
 
   logger.error(error.message)
